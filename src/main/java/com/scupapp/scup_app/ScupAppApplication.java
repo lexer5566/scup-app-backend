f1fa8cb3006db27f1e4915e722d965ef12da694c
Miroslav Radics <radicsmiros22@gmail.com>
2026-02-02 17:09:34 +0100

    Initial commit

---
+++ src/main/java/com/scupapp/scup_app/ScupAppApplication.java
@@ -0,0 +1,13 @@
+package com.scupapp.scup_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScupAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScupAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scupapp/scup_app/ScupAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scupapp.scup_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScupAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
